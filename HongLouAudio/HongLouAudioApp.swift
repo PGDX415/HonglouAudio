535cc9b13acbbb38696348a85d545e34cdb327dd
PaulGDX <PGONG415@GMAIL.COM>
2026-03-30 21:41:37 +0800

    Initial Commit

---
+++ HongLouAudio/HongLouAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongLouAudioApp.swift
+//  HongLouAudio
+//
+//  Created by Paul Dexin Gong on 2026/3/30.
+//
+
+import SwiftUI
+
+@main
+struct HongLouAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
